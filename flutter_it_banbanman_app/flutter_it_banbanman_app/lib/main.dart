008df7a5d1176708e61aac58e15f5c2841bc5da9
Tony Liao <tonynowater87@gmail.com>
2019-10-02 21:29:30 +0800

    use onGenerateRoute and routes to navigate page

--- flutter_it_banbanman_app/flutter_it_banbanman_app/lib/main.dart
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_it_banbanman_app/routes.dart';
 
 void main() => runApp(GitmeRebornApp());
 
@@ -7,15 +8,27 @@ class GitmeRebornApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gitmme Reborn',
+      title: "Gitmme Reborn",
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: LoginPage(),
+      routes: {
+        GitmeRebornRoutes.login: (context) => LoginPage(),
+        GitmeRebornRoutes.home: (context) => MainPage(),
+      },
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case GitmeRebornRoutes.root:
+            return MaterialPageRoute(builder: (context) => LoginPage());
+          default:
+            return MaterialPageRoute(builder: (context) => LoginPage());
+        }
+      },
     );
   }
 }
 
+// 登入頁面
 class LoginPage extends StatelessWidget {
   LoginPage({Key key}) : super(key: key);
 
@@ -23,7 +36,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text("Login"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -32,27 +45,51 @@ class LoginPage extends StatelessWidget {
               padding: EdgeInsets.all(20),
               child: TextFormField(
                 decoration: InputDecoration(
-                    labelText: 'Name',
+                    labelText: "Name",
                     prefixIcon: Icon(Icons.people),
-                    hintText: 'Your Github account user name.'),
+                    hintText: "Your Github account user name."),
               )),
           Padding(
               padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
               child: TextFormField(
                 decoration: InputDecoration(
-                    labelText: 'Password',
+                    labelText: "Password",
                     prefixIcon: Icon(Icons.lock),
-                    hintText: 'Your Github account password ...'),
+                    hintText: "Your Github account password ..."),
               )),
           SizedBox(
             width: 200,
             height: 50,
             child: FlatButton(
-              onPressed: () {},
-              child: Text('Login'),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, GitmeRebornRoutes.home);
+              },
+              child: Text("Login"),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+// 主頁面
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: TabBar(
+            tabs: <Widget>[
+              Tab(text: "Home"),
+              Tab(text: "Repo"),
+              Tab(text: "Activity"),
+              Tab(text: "Issues"),
+            ],
+          ),
+        ),
       ),
     );
   }
