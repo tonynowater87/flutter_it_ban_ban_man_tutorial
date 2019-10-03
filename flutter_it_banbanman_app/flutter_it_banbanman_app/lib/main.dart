096aedaf0691664f0c005f3c7f5d1ce6141587ff
Tony Liao <tonynowater87@gmail.com>
2019-10-03 11:11:03 +0800

    use ProgressHUD

--- flutter_it_banbanman_app/flutter_it_banbanman_app/lib/main.dart
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_it_banbanman_app/module/login/LoginPage.dart';
 import 'package:flutter_it_banbanman_app/routes.dart';
 
 void main() => runApp(GitmeRebornApp());
@@ -25,62 +26,6 @@ class GitmeRebornApp extends StatelessWidget {
             return MaterialPageRoute(builder: (context) => LoginPage());
         }
       },
-    );
-  }
-}
-
-// 登入頁面
-class LoginPage extends StatelessWidget {
-  LoginPage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          alignment: Alignment.centerLeft,
-          child: Text("登入"),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: 25,
-          ),
-          Padding(
-              padding: EdgeInsets.all(20),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Name",
-                    prefixIcon: Icon(Icons.people),
-                    hintText: "Your Github account user name."),
-              )),
-          Padding(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.lock),
-                  hintText: "Your Github account password ...",
-                ),
-              )),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            width: 200,
-            height: 50,
-            color: Colors.black12,
-            child: FlatButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, RoutesTable.home);
-              },
-              child: Text("Login"),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
---
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/login/LoginPage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_it_banbanman_app/routes.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
+
+// 登入頁面
+class LoginPage extends StatelessWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ProgressHUD(
+        child: Builder(
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: Container(
+            alignment: Alignment.centerLeft,
+            child: Text("登入"),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Name",
+                      prefixIcon: Icon(Icons.people),
+                      hintText: "Your Github account user name."),
+                )),
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    hintText: "Your Github account password ...",
+                  ),
+                )),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: 200,
+              height: 50,
+              color: Colors.black12,
+              child: FlatButton(
+                child: Text("登入"),
+                onPressed: () {
+                  final progress = ProgressHUD.of(context);
+                  progress.showWithText("Loading...");
+                  Future.delayed(Duration(seconds: 2), () {
+                    Navigator.pushReplacementNamed(context, "/home");
+                    progress.dismiss();
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
