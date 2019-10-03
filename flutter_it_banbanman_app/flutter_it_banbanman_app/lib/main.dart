422ce1bcc08143a33f3a314b5f89bab668a0341b
Tony Liao <tonynowater87@gmail.com>
2019-10-03 15:34:41 +0800

    use RefreshIndicator to add repo list

--- flutter_it_banbanman_app/flutter_it_banbanman_app/lib/main.dart
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/main.dart
@@ -136,61 +136,69 @@ class MainPage extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text(
-              "最新",
-              style: TextStyle(fontSize: 20, color: Colors.red),
+    return RefreshIndicator(
+      onRefresh: () {},
+      child: Container(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                "最新",
+                style: TextStyle(fontSize: 20, color: Colors.red),
+              ),
             ),
-          ),
-          Divider(
-            height: 0,
-            thickness: 0,
-            color: Colors.blueGrey,
-          ),
-          ListTile(
-            title: Text(
-              "歡迎使用Gitme v1.1.1",
-              style: TextStyle(fontSize: 20, color: Colors.black),
+            Divider(
+              height: 0,
+              thickness: 0,
+              color: Colors.blueGrey,
             ),
-            subtitle: Text("每一位用戶都是gitme的主人!"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () => {},
-          ),
-          Divider(
-            height: 0,
-            thickness: 10,
-            color: Colors.grey,
-          ),
-          ListTile(
-            title: Text(
-              "推薦項目",
-              style: TextStyle(fontSize: 20, color: Colors.green),
+            ListTile(
+              title: Text(
+                "歡迎使用Gitme v1.1.1",
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              subtitle: Text("每一位用戶都是gitme的主人!"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () => {},
             ),
-          ),
-          Divider(
-            height: 0,
-            thickness: 0,
-            color: Colors.blueGrey,
-          ),
-          ListTile(
-            title: Text(
-              "flukit",
-              style: TextStyle(fontSize: 20, color: Colors.black),
+            Divider(
+              height: 0,
+              thickness: 10,
+              color: Colors.grey,
             ),
-            subtitle: Text("A Flutter UI Kit"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () => {},
-          ),
-        ],
+            ListTile(
+              title: Text(
+                "推薦項目",
+                style: TextStyle(fontSize: 20, color: Colors.green),
+              ),
+            ),
+            Divider(
+              height: 0,
+              thickness: 0,
+              color: Colors.blueGrey,
+            ),
+            ListTile(
+              title: Text(
+                "flukit",
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              subtitle: Text("A Flutter UI Kit"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () => {},
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class RepoPage extends StatelessWidget {
+class RepoPage extends StatefulWidget {
+  @override
+  _RepoPageState createState() => _RepoPageState();
+}
+
+class _RepoPageState extends State<RepoPage> {
   final repoList = [
     {
       "title": "BbsonLin/gitme_reborn",
@@ -217,22 +225,36 @@ class RepoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.separated(
-          itemBuilder: (BuildContext context, int index) => ListTile(
-                title: Text(repoList[index]["title"]),
-                subtitle: Text(repoList[index]["description"]),
-                trailing: Text(repoList[index]["lang"]),
-                isThreeLine: false,
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                onTap: () {},
-              ),
-          separatorBuilder: (BuildContext context, int index) => Divider(
-                height: 0,
-                color: Colors.grey,
-              ),
-          itemCount: repoList.length),
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future.delayed(Duration(seconds: 1), () {
+          setState(() {
+            repoList.add({
+              "title": "refresh new item",
+              "description": "\n\n★ 3",
+              "lang": "● Kotlin"
+            });
+            print('[Tony] onRefresh ${repoList.length}');
+          });
+        });
+      },
+      child: Container(
+        child: ListView.separated(
+            itemBuilder: (BuildContext context, int index) => ListTile(
+                  title: Text(repoList[index]["title"]),
+                  subtitle: Text(repoList[index]["description"]),
+                  trailing: Text(repoList[index]["lang"]),
+                  isThreeLine: false,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                  onTap: () {},
+                ),
+            separatorBuilder: (BuildContext context, int index) => Divider(
+                  height: 0,
+                  color: Colors.grey,
+                ),
+            itemCount: repoList.length),
+      ),
     );
   }
 }
