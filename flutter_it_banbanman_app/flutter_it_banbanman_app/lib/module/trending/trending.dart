3535b4505a657d7c3f371b44adadf87875f8597b
Tony Liao <tonynowater87@gmail.com>
2019-10-17 16:27:11 +0800

    add TrendingDeveloper widget

--- flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/trending/trending.dart
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/trending/trending.dart
@@ -33,7 +33,7 @@ class TrendingPage extends StatelessWidget {
           body: TabBarView(
             children: <Widget>[
               TrendingProject(),
-              TrendingProject(),
+              TrendingDeveloper(),
             ],
           ),
         ));
@@ -45,10 +45,10 @@ class TrendingProject extends StatelessWidget {
 
   TrendingProject({Key key}) : super(key: key) {
     final circleAvatar = {
-          "username": "PavelDoGreat",
-          "href": "https://github.com/PavelDoGreat",
-          "avatar": "https://avatars2.githubusercontent.com/u/24439787"
-        };
+      "username": "PavelDoGreat",
+      "href": "https://github.com/PavelDoGreat",
+      "avatar": "https://avatars2.githubusercontent.com/u/24439787"
+    };
 
     final project1 = {
       "title": "google / flutter",
@@ -80,30 +80,36 @@ class TrendingProject extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (context, index) {
-          return ListTile(
-              title: Text(trendingProject[index]["title"]),
-              subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 4.0),
-                    Text(trendingProject[index]["subTitle"]),
-                    SizedBox(height: 4.0),
-                    Row(
-                      children: <Widget>[
-                        Text("★ ${trendingProject[index]['starts']}"),
-                        // ignore: sdk_version_ui_as_code
-                        ...buildList(trendingProject[index]["builtBy"])
-                      ],
-                    )
-                  ]),
-              trailing: Text(trendingProject[index]["trailing"]));
+    return RefreshIndicator(
+        onRefresh: () {
+          return Future.delayed(Duration(seconds: 2), () {});
         },
-        separatorBuilder: (context, index) {
-          return Divider();
-        },
-        itemCount: trendingProject.length);
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(trendingProject[index]["title"]),
+                subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 4.0),
+                      Text(trendingProject[index]["subTitle"]),
+                      SizedBox(height: 4.0),
+                      Row(
+                        children: <Widget>[
+                          Text("★ ${trendingProject[index]['starts']}"),
+                          // ignore: sdk_version_ui_as_code
+                          ...buildList(trendingProject[index]["builtBy"])
+                        ],
+                      )
+                    ]),
+                trailing: Text(trendingProject[index]["trailing"]),
+                onTap: () {},
+              );
+            },
+            separatorBuilder: (context, index) {
+              return Divider(height: 0);
+            },
+            itemCount: trendingProject.length));
   }
 
   List<Padding> buildList(List buildByList) {
@@ -123,5 +129,79 @@ class TrendingProject extends StatelessWidget {
     } else {
       return circleAvatars;
     }
+  }
+}
+
+class TrendingDeveloper extends StatelessWidget {
+  final List developers = [
+    {
+      "username": "lirantal",
+      "name": "Liran Tal",
+      "type": "user",
+      "url": "https://github.com/lirantal",
+      "avatar": "https://avatars2.githubusercontent.com/u/316371",
+      "repo": {
+        "name": "dockly",
+        "description":
+            "Immersive terminal interface for managing docker containers and services",
+        "url": "https://github.com/lirantal/dockly"
+      }
+    },
+    {
+      "username": "wojtekmaj",
+      "name": "Wojciech Maj",
+      "type": "user",
+      "url": "https://github.com/wojtekmaj",
+      "avatar": "https://avatars0.githubusercontent.com/u/5426427",
+      "repo": {
+        "name": "react-pdf",
+        "description":
+            "Display PDFs in your React app as easily as if they were images.",
+        "url": "https://github.com/wojtekmaj/react-pdf"
+      }
+    },
+    {
+      "username": "mrdoob",
+      "name": "Mr.doob",
+      "type": "user",
+      "url": "https://github.com/mrdoob",
+      "avatar": "https://avatars2.githubusercontent.com/u/97088",
+      "repo": {
+        "name": "three.js",
+        "description": "JavaScript 3D library.",
+        "url": "https://github.com/mrdoob/three.js"
+      }
+    }
+  ];
+
+  TrendingDeveloper({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future.delayed(Duration(seconds: 2), () {});
+      },
+      child: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              contentPadding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(developers[index]["avatar"]),
+              ),
+              title: Row(children: [
+                Text(developers[index]["name"]),
+                SizedBox(width: 16),
+                Text(developers[index]['username'])
+              ]),
+              subtitle: Text(developers[index]["repo"]["description"]),
+              onTap: () {},
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider(height: 0);
+          },
+          itemCount: developers.length),
+    );
   }
 }
