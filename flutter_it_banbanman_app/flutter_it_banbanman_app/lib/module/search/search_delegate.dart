eae882863b5aa0f237ef759b61bebaa0589653e3
Tony Liao <tonynowater87@gmail.com>
2019-10-24 17:56:17 +0800

    update MySearchDelegate

--- flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/search/search_delegate.dart
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/search/search_delegate.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_it_banbanman_app/module/api/github_api.dart';
+import 'package:github/server.dart';
 
 enum SearchTypes { repos, users }
 
 class MySearchDelegate extends SearchDelegate {
   SearchTypes _searchTypes = SearchTypes.repos;
+  Future<List<User>> users;
+  Future<List<Repository>> repos;
 
   @override
   ThemeData appBarTheme(BuildContext context) {
@@ -11,7 +15,8 @@ class MySearchDelegate extends SearchDelegate {
   }
 
   @override
-  List<Widget> buildActions(BuildContext context) {  // 搜尋頁面右邊功能按鈕區
+  List<Widget> buildActions(BuildContext context) {
+    // 搜尋頁面右邊功能按鈕區
     return [
       IconButton(
         icon: Icon(Icons.clear),
@@ -25,6 +30,7 @@ class MySearchDelegate extends SearchDelegate {
           showSuggestions(context); //update suggestion
         },
         itemBuilder: (BuildContext context) {
+          // 預設的搜尋主頁面
           return [
             CheckedPopupMenuItem(
                 value: SearchTypes.repos,
@@ -41,35 +47,165 @@ class MySearchDelegate extends SearchDelegate {
   }
 
   @override
-  Widget buildLeading(BuildContext context) { // 搜尋頁面返回鍵
-    return IconButton(
-      icon: Icon(Icons.arrow_back_ios),
-      onPressed: () {
-        close(context, null);
+  Widget buildLeading(BuildContext context) {
+    // 搜尋頁面返回鍵
+    return BackButton();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // 搜尋建議頁面
+    switch (_searchTypes) {
+      case SearchTypes.users:
+        {
+          return Center(child: Text("Search users that contain '$query' ..."));
+        }
+      case SearchTypes.repos:
+        return Center(child: Text("Search repos that contain '$query' ..."));
+    }
+
+    throw Exception("Illegal SearchType:$_searchTypes");
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    print('[Tony] buildResults');
+    // 搜尋結果頁面
+    return SearchResultPage(
+      query: query,
+      searchTypes: _searchTypes,
+    );
+  }
+}
+
+class SearchResultPage extends StatefulWidget {
+  final String query;
+  final SearchTypes searchTypes;
+
+  SearchResultPage({Key key, @required this.query, @required this.searchTypes})
+      : super(key: key);
+
+  @override
+  _SearchResultPageState createState() => _SearchResultPageState();
+}
+
+class _SearchResultPageState extends State<SearchResultPage> {
+  Future<List> searchResults;
+
+  @override
+  void initState() {
+    super.initState();
+    switch (widget.searchTypes) {
+      case SearchTypes.users:
+        searchResults = searchUsers(widget.query);
+        break;
+      case SearchTypes.repos:
+        searchResults = searchRepos(widget.query);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: searchResults,
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            if (!snapshot.hasData) {
+              return Center(
+                child: Text("No Data"),
+              );
+            }
+            return ListView.separated(
+              // ignore: missing_return
+            itemBuilder: (BuildContext context, int index) {
+                  switch (widget.searchTypes) {
+                    case SearchTypes.repos:
+                      final data = snapshot.data.cast<Repository>()[index];
+                      return RepoTile(
+                        title: data.fullName,
+                        subTitle: data.description,
+                        stars: data.stargazersCount,
+                        language: data.language,
+                      );
+                    case SearchTypes.users:
+                      final data = snapshot.data.cast<User>()[index];
+                      return UserTile(
+                        avatarUrl: data.avatarUrl,
+                        userName: data.login,
+                      );
+                  }
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Divider(
+                    height: 0,
+                  );
+                },
+                itemCount: snapshot.data.length);
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+          case ConnectionState.active:
+            break;
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
       },
     );
   }
 
-  @override
-  Widget buildResults(BuildContext context) { // 搜尋結果頁面
-    return Text("buildResults:($query, $_searchTypes)");
+  Future<List<User>> searchUsers(String query) async {
+    return gitHubClient.search.users(query, pages: 1).toList();
   }
 
-  @override
-  Widget buildSuggestions(BuildContext context) { // 搜尋建議頁面
-    var searchPrefix;
-    switch (_searchTypes) {
-      case SearchTypes.repos:
-        searchPrefix = "Search Repo:";
-        break;
-      case SearchTypes.users:
-        searchPrefix = "Search User:";
-        break;
-    }
+  Future<List<Repository>> searchRepos(String query) async {
+    return gitHubClient.search.repositories(query, pages: 1).toList();
+  }
+}
 
-    return Container(
-      alignment: Alignment.center,
-      child: Text(searchPrefix + " " + query),
+class RepoTile extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final String language;
+  final int stars;
+
+  RepoTile({Key key, this.title, this.subTitle, this.stars, this.language})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final description = subTitle ?? "No description provided";
+
+    return ListTile(
+      title: Text(title),
+      subtitle: Text("$description\n★ $stars"),
+      trailing: Text(language),
+      onTap: () {},
+    );
+  }
+}
+
+class UserTile extends StatelessWidget {
+  final String avatarUrl;
+  final String userName;
+
+  UserTile({Key key, this.avatarUrl, this.userName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(avatarUrl),
+      ),
+      title: Text(userName),
+      onTap: () {},
     );
   }
 }
