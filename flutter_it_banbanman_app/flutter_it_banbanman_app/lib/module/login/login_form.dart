7fa17ea042a8e660e465b8fe2ef49a1b061ba199
Tony Liao <tonynowater87@gmail.com>
2019-11-04 14:29:24 +0800

    Fix the bottom of login page renderflex overflowed problem

--- flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/login/login_form.dart
+++ flutter_it_banbanman_app/flutter_it_banbanman_app/lib/module/login/login_form.dart
@@ -33,7 +33,7 @@ class _LoginFormWidgetState extends State<LoginFormWidget> {
     super.didUpdateWidget(oldWidget);
     _sp.getAutoLogin().then((autoLogin) {
       print('[Tony] auto login value:$autoLogin');
-      if(autoLogin != null) {
+      if (autoLogin != null) {
         setState(() {
           widget._autoLogin = autoLogin;
         });
@@ -43,91 +43,96 @@ class _LoginFormWidgetState extends State<LoginFormWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Image.asset("assets/images/login_sign.png"),
-          TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return S.of(context).login_username_error_msg;
-              } else {
-                return null;
-              }
-            },
-            controller: userNameController,
-            decoration: InputDecoration(
-                labelText: S.of(context).login_username,
-                prefixIcon: Icon(Icons.people),
-                hintText: S.of(context).login_username_hint),
-            onFieldSubmitted: (value) {
-              FocusScope.of(context).requestFocus(_passwordFocusNode);
-            },
-          ),
-          SizedBox(
-            height: 0,
-          ),
-          TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return S.of(context).login_password_error_msg;
-              } else {
-                return null;
-              }
-            },
-            focusNode: _passwordFocusNode,
-            controller: passwordController,
-            obscureText: _isHidden,
-            decoration: InputDecoration(
-              labelText: S.of(context).login_password,
-              prefixIcon: Icon(Icons.lock),
-              suffixIcon: IconButton(
-                  icon: _isHidden
-                      ? Icon(Icons.visibility_off)
-                      : Icon(Icons.visibility),
-                  onPressed: _toggleVisibility),
-              hintText: S.of(context).login_password_hint,
-            ),
-            onFieldSubmitted: (value) {
-              widget.onLogin(this);
-            },
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            width: double.infinity,
-            height: 50,
-            color: Colors.black12,
-            child: FlatButton(
-              child: Text(S.of(context).login),
-              onPressed: () {
-                widget.onLogin(this);
-              },
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+    return SingleChildScrollView(
+      child: Form(
+        key: formKey,
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Text(S.of(context).login_auto_login),
-              Checkbox(
-                value: widget._autoLogin,
-                onChanged: (value) {
-                  setState(() {
-                    widget._autoLogin = !widget._autoLogin;
-                    _sp.setAutoLogin(widget._autoLogin);
-                    if (widget._autoLogin) {
-                      _sp.setUserName(userNameController.text);
-                      _sp.setUserPwd(passwordController.text);
-                    }
-                  });
+              Image.asset("assets/images/login_sign.png"),
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return S.of(context).login_username_error_msg;
+                  } else {
+                    return null;
+                  }
                 },
+                controller: userNameController,
+                decoration: InputDecoration(
+                    labelText: S.of(context).login_username,
+                    prefixIcon: Icon(Icons.people),
+                    hintText: S.of(context).login_username_hint),
+                onFieldSubmitted: (value) {
+                  FocusScope.of(context).requestFocus(_passwordFocusNode);
+                },
+              ),
+              SizedBox(
+                height: 0,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return S.of(context).login_password_error_msg;
+                  } else {
+                    return null;
+                  }
+                },
+                focusNode: _passwordFocusNode,
+                controller: passwordController,
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                  labelText: S.of(context).login_password,
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                      icon: _isHidden
+                          ? Icon(Icons.visibility_off)
+                          : Icon(Icons.visibility),
+                      onPressed: _toggleVisibility),
+                  hintText: S.of(context).login_password_hint,
+                ),
+                onFieldSubmitted: (value) {
+                  widget.onLogin(this);
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                color: Colors.black12,
+                child: FlatButton(
+                  child: Text(S.of(context).login),
+                  onPressed: () {
+                    widget.onLogin(this);
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(S.of(context).login_auto_login),
+                  Checkbox(
+                    value: widget._autoLogin,
+                    onChanged: (value) {
+                      setState(() {
+                        widget._autoLogin = !widget._autoLogin;
+                        _sp.setAutoLogin(widget._autoLogin);
+                        if (widget._autoLogin) {
+                          _sp.setUserName(userNameController.text);
+                          _sp.setUserPwd(passwordController.text);
+                        }
+                      });
+                    },
+                  )
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
